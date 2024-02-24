b55bcd4525e976afc7ed8fce80a2e76b541ad027
Brandon Castro <C1brandon626@gmail.com>
2024-02-24 03:18:29 +0000

    finished with part d and question 2 part a

--- src/Hw3.hs
+++ src/Hw3.hs
@@ -1,33 +1,31 @@
-{- | CSE 116: All about fold.
-
-     For this assignment, you may use the following library functions:
-
-     length
-     append (++)
-     map
-     foldl'
-     foldr
-     unzip
-     zip
-     reverse
-
-  Use www.haskell.org/hoogle to learn more about the above.
-
-  Do not change the skeleton code! The point of this assignment
-  is to figure out how the functions can be written this way
-  (using fold). You may only replace the `error "TBD:..."` terms.
-
--}
-
+-- | CSE 116: All about fold.
+--
+--     For this assignment, you may use the following library functions:
+--
+--     length
+--     append (++)
+--     map
+--     foldl'
+--     foldr
+--     unzip
+--     zip
+--     reverse
+--
+--  Use www.haskell.org/hoogle to learn more about the above.
+--
+--  Do not change the skeleton code! The point of this assignment
+--  is to figure out how the functions can be written this way
+--  (using fold). You may only replace the `error "TBD:..."` terms.
 module Hw3 where
 
-import Prelude hiding (replicate, sum)
 import Data.List (foldl')
+import Prelude hiding (replicate, sum)
 
 foldLeft :: (a -> b -> a) -> a -> [b] -> a
 foldLeft = foldl'
 
 --------------------------------------------------------------------------------
+
 -- | sqSum [x1, ... , xn] should return (x1^2 + ... + xn^2)
 --
 -- >>> sqSum []
@@ -38,14 +36,14 @@ foldLeft = foldl'
 --
 -- >>> sqSum [(-1), (-2), (-3), (-4)]
 -- 30
-
 sqSum :: [Int] -> Int
 sqSum xs = foldLeft f base xs
   where
-   f a x = a + x * x
-   base  = 0
+    f a x = a + x * x
+    base = 0
 
 --------------------------------------------------------------------------------
+
 -- | `pipe [f1,...,fn] x` should return `f1(f2(...(fn x)))`
 --
 -- >>> pipe [] 3
@@ -56,14 +54,14 @@ sqSum xs = foldLeft f base xs
 --
 -- >>> pipe [(\x -> x * 4), (\x -> x + x)] 3
 -- 24
-
 pipe :: [(a -> a)] -> (a -> a)
-pipe fs   = foldLeft f base fs
+pipe fs = foldLeft f base fs
   where
     f a x = a . x
-    base  = id
+    base = id
 
 --------------------------------------------------------------------------------
+
 -- | `sepConcat sep [s1,...,sn]` returns `s1 ++ sep ++ s2 ++ ... ++ sep ++ sn`
 --
 -- >>> sepConcat "---" []
@@ -74,19 +72,19 @@ pipe fs   = foldLeft f base fs
 --
 -- >>> sepConcat "#" ["a","b","c","d","e"]
 -- "a#b#c#d#e"
-
 sepConcat :: String -> [String] -> String
-sepConcat sep []     = ""
-sepConcat sep (x:xs) = foldLeft f base l
+sepConcat sep [] = ""
+sepConcat sep (x : xs) = foldLeft f base l
   where
-    f a x            = a ++ sep ++ x
-    base             = x
-    l                = xs
+    f a x = a ++ sep ++ x
+    base = x
+    l = xs
 
 intString :: Int -> String
 intString = show
 
 --------------------------------------------------------------------------------
+
 -- | `stringOfList pp [x1,...,xn]` uses the element-wise printer `pp` to
 --   convert the element-list into a string:
 --
@@ -98,11 +96,11 @@ intString = show
 --
 -- >>> stringOfList (stringOfList show) [[1, 2, 3], [4, 5], [6], []]
 -- "[[1, 2, 3], [4, 5], [6], []]"
-
 stringOfList :: (a -> String) -> [a] -> String
-stringOfList f xs = error "TBD:stringOfList"
+stringOfList f xs = "[" ++ sepConcat ", " (map f xs) ++ "]"
 
 --------------------------------------------------------------------------------
+
 -- | `clone x n` returns a `[x,x,...,x]` containing `n` copies of `x`
 --
 -- >>> clone 3 5
@@ -110,13 +108,13 @@ stringOfList f xs = error "TBD:stringOfList"
 --
 -- >>> clone "foo" 2
 -- ["foo", "foo"]
-
 clone :: a -> Int -> [a]
-clone x n = error "TBD:clone"
+clone x n = [x | _ <- [1 .. n]]
 
 type BigInt = [Int]
 
 --------------------------------------------------------------------------------
+
 -- | `padZero l1 l2` returns a pair (l1', l2') which are just the input lists,
 --   padded with extra `0` on the left such that the lengths of `l1'` and `l2'`
 --   are equal.
@@ -126,11 +124,15 @@ type BigInt = [Int]
 --
 -- >>> padZero [1,0,0,2] [9,9]
 -- ([1,0,0,2], [0,0,9,9])
-
 padZero :: BigInt -> BigInt -> (BigInt, BigInt)
-padZero l1 l2 = error "TBD:padZero"
+padZero l1 l2 =
+  let diff = abs (length l1 - length l2)
+   in if length l1 > length l2
+        then (l1, clone 0 diff ++ l2)
+        else (clone 0 diff ++ l1, l2)
 
 --------------------------------------------------------------------------------
+
 -- | `removeZero ds` strips out all leading `0` from the left-side of `ds`.
 --
 -- >>> removeZero [0,0,0,1,0,0,2]
@@ -141,12 +143,14 @@ padZero l1 l2 = error "TBD:padZero"
 --
 -- >>> removeZero [0,0,0,0]
 -- []
-
 removeZero :: BigInt -> BigInt
-removeZero ds = error "TBD:removeZero"
-
+removeZero ds = foldLeft f base ds
+  where
+    f a x = if null a && x == 0 then [] else a ++ [x]
+    base = []
 
 --------------------------------------------------------------------------------
+
 -- | `bigAdd n1 n2` returns the `BigInt` representing the sum of `n1` and `n2`.
 --
 -- >>> bigAdd [9, 9] [1, 0, 0, 2]
@@ -154,28 +158,27 @@ removeZero ds = error "TBD:removeZero"
 --
 -- >>> bigAdd [9, 9, 9, 9] [9, 9, 9]
 -- [1, 0, 9, 9, 8]
-
 bigAdd :: BigInt -> BigInt -> BigInt
-bigAdd l1 l2     = removeZero res
+bigAdd l1 l2 = removeZero res
   where
-    (l1', l2')   = padZero l1 l2
-    res          = foldLeft f base args
-    f a x        = error "TBD:bigAdd:f"
-    base         = error "TBD:bigAdd:base"
-    args         = error "TBD:bigAdd:args"
-
+    (l1', l2') = padZero l1 l2
+    res = foldLeft f base args
+    f a x = error "TBD:bigAdd:f"
+    base = error "TBD:bigAdd:base"
+    args = error "TBD:bigAdd:args"
 
 --------------------------------------------------------------------------------
+
 -- | `mulByInt i n` returns the result of multiplying
 --   the int `i` with `BigInt` `n`.
 --
 -- >>> mulByInt 9 [9,9,9,9]
 -- [8,9,9,9,1]
-
 mulByInt :: Int -> BigInt -> BigInt
 mulByInt i n = error "TBD:mulByInt"
 
 --------------------------------------------------------------------------------
+
 -- | `bigMul n1 n2` returns the `BigInt` representing the product of `n1` and `n2`.
 --
 -- >>> bigMul [9,9,9,9] [9,9,9,9]
@@ -183,11 +186,10 @@ mulByInt i n = error "TBD:mulByInt"
 --
 -- >>> bigMul [9,9,9,9,9] [9,9,9,9,9]
 -- [9,9,9,9,8,0,0,0,0,1]
-
 bigMul :: BigInt -> BigInt -> BigInt
 bigMul l1 l2 = res
   where
     (_, res) = foldLeft f base args
-    f a x    = error "TBD:bigMul:f"
-    base     = error "TBD:bigMul:base"
-    args     = error "TBD:bigMul:args"
+    f a x = error "TBD:bigMul:f"
+    base = error "TBD:bigMul:base"
+    args = error "TBD:bigMul:args"
